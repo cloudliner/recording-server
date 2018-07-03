1f858f58e096a9199914395987359777b8fc5264
cloudliner <cloudliner.jp@gmail.com>
2018-07-03 21:42:59 +0900

    add puppeteer navigation

--- src/server.ts
+++ src/server.ts
@@ -11,19 +11,68 @@ function defaultListener(request: express.Request, response: express.Response) {
 
 server.get("/", defaultListener);
 
-function runListener(request: express.Request, response: express.Response) {
+function recordListener(request: express.Request, response: express.Response) {
   console.log('Launch Chrome');
   (async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(`http://localhost:${portNumber}/`);
+
+    page.on('console', (c) => {
+      console.log(`${c.type()}: ${c.text()}: ${c.args()}`);
+    });
+
+    await page.goto(`http://localhost:4200/`);
+    await page.waitFor(500);
+
+    await page.screenshot({path: 'Capture-01-initial.png', fullPage: true});
+
+    await page.select('#room', 'test-room');
+    await page.waitFor(500);
+    await page.screenshot({path: 'Capture-02-select.png', fullPage: true});
+
+    const join = await page.$('#join');
+    if (join) {
+      join.click();
+    }
+    await page.waitFor(500);
+    await page.screenshot({path: 'Capture-03-join.png', fullPage: true});
+
+    const record = await page.$('#record');
+    if (record) {
+      record.click();
+    }
+    await page.waitFor(500);
+    await page.screenshot({path: 'Capture-04-record-start.png', fullPage: true});
+
+    await page.waitFor(10000);
+    if (record) {
+      record.click();
+    }
+    await page.screenshot({path: 'Capture-05-record-stop.png', fullPage: true});
+
+    await page.waitFor(10000);
+
+    const download = await page.$('#download');
+    if (download) {
+      download.click();
+    }
+    await page.waitFor(500);
+    await page.screenshot({path: 'Capture-06-download.png', fullPage: true});
+
+    const exit = await page.$('#exit');
+    if (exit) {
+      exit.click();
+    }
+    await page.waitFor(500);
+    await page.screenshot({path: 'Capture-07-exit.png', fullPage: true});
+
     console.log('Close Chrome');
     await browser.close();
     response.send('Launch Chrome');
   })();
 }
 
-server.get("/run", runListener);
+server.get("/record", recordListener);
 
 server.listen(portNumber, () => {
   console.log(`Listening on localhost:${ portNumber }`);
