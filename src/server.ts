cda542d6fae892faeacba8921b892866bb687e90
cloudliner <cloudliner.jp@gmail.com>
2018-07-04 23:16:19 +0900

    change log

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,8 @@ import express = require('express');
 import puppeteer = require('puppeteer');
 
 /*
-declare class puppeteer {
-  dePage {
-  _client: any;
-};
-*/
+declare var puppeteer.Page._client: any;
+ */
 
 const server = express();
 const portNumber = 8080;
@@ -19,50 +16,47 @@ function defaultListener(request: express.Request, response: express.Response) {
 server.get("/", defaultListener);
 
 function recordListener(request: express.Request, response: express.Response) {
-  console.log('Launch Chrome');
-  response.write(`Launch Chrome`);
+  const l = new Logger(response);
   (async() => {
+    await l.log('Launch Chrome');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    l.page = page;
     await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: './' });
 
     page.on('console', (c) => {
-      console.log(`${c.type()}: ${c.text()}: ${c.args()}`);
+      l.log(`${c.type()}: ${c.text()}: ${c.args()}`);
     });
 
     await page.goto(`http://localhost:4200/`);
+    await page.waitFor(1000);
 
     const recordOnly = await page.$('#record-only');
     if (recordOnly) {
       recordOnly.click();
     }
-    await page.screenshot({path: 'capture-01-initial.png', fullPage: true});
-    response.write(`initial`);
+    await l.log('initial', true);
 
     await page.select('#room', 'test-room');
-    await page.screenshot({path: 'capture-02-select.png', fullPage: true});
-    response.write(`select`);
+    await l.log('select', true);
 
     const join = await page.$('#join');
     if (join) {
       join.click();
     }
-    await page.screenshot({path: 'capture-03-join.png', fullPage: true});
-    response.write(`join`);
+    await l.log('join', true);
 
     const record = await page.$('#record');
     if (record) {
       record.click();
     }
-    await page.screenshot({path: 'capture-04-record-start.png', fullPage: true});
-    response.write(`record-start`);
+    await l.log('record-start', true);
 
     await page.waitFor(5000);
     if (record) {
       record.click();
     }
-    await page.screenshot({path: 'capture-05-record-stop.png', fullPage: true});
-    response.write(`record-stop`);
+    await l.log('record-stop', true);
 
     await page.waitFor('#download');
 
@@ -70,21 +64,34 @@ function recordListener(request: express.Request, response: express.Response) {
     if (download) {
       download.click();
     }
-    await page.screenshot({path: 'capture-06-download.png', fullPage: true});
-    response.write(`download`);
+    await l.log('download', true);
 
     const exit = await page.$('#exit');
     if (exit) {
       exit.click();
     }
-    await page.screenshot({path: 'capture-07-exit.png', fullPage: true});
-    response.write(`exit`);
+    await l.log('exit', true);
 
     await browser.close();
-    console.log('Close Chrome');
-    response.write(`Close Chrome`);
+    await l.log('Close Chrome');
     response.end();
   })();
+}
+
+class Logger {
+  private startTime = Date.now();
+  private count = 0;
+  page: any;
+
+  constructor(private response: express.Response) { }
+
+  async log(message: string, capture = false) {
+    this.response.write(`${(Date.now() - this.startTime) / 1000}: ${message}\n`);
+    console.log(message);
+    if (capture && this.page) {
+      await this.page.screenshot({path: `capture-${++ this.count}-${message}.png`, fullPage: true});
+    }
+  }
 }
 
 server.get("/record", recordListener);
